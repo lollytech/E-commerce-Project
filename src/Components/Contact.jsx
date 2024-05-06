03c9e449eca429d8f24943514eac00111db46ff0
Adebayo Sukurat Omolara <127861458+lollytech@users.noreply.github.com>
2024-05-06 19:08:41 +0100

    changes in signup.jsx and login page

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Contact.css"
+
+const Contact = () => {
+  return (
+    <div className= "contact">
+      <div className = "contact-style">
+      Contact us 
+      </div>
+    </div>
+
+  )
+};
+
+export default Contact;
