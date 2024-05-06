2330b53ca9cdb9d543d2ef6c7ac9662213166b79
Adebayo Sukurat Omolara <127861458+lollytech@users.noreply.github.com>
2024-05-06 19:14:24 +0100

    changes on signup and login page

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Signup.css"
+
 
 const Signup = () => {
-  return 
-  <div>
-
-  </div>;
+    const [username, setusername] = useState("");
+    const [pin, setpin] = useState("");
+    const [error, seterror] = useState("");
+  
+    const handlesubmit = (event) => {
+      event.preventDefault();
+    };
+  
+    const valuehandler = (event) => {
+      // console.log(event);
+      setusername(event.target.value);
+      console.log(event.target.value);
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handlesubmit} className="sign-up-form" action="">
+          <label>
+            Username
+            <input className="inputs" type="text" value={username} onChange={valuehandler} />
+          </label>
+          <label>
+            Email
+            <input className="inputs"
+            type="email" name="" id="" />
+          </label>
+          <label>
+            Password
+            <input className="inputs"
+              type="text"
+              value={pin}
+              onChange={(event) => setpin(event.target.value)}
+            />
+          </label>
+          <br />
+          <button className="signupbtn" type="submit">
+            Signup
+          </button>
+          <div className="error">{error && <div>{error}</div>}</div>
+        </form>
+      </div>
+    )
 };
+
+
 
 export default Signup;
